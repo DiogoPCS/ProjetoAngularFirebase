93225da673fbff7ba967611c58eb9c4823660953
victorsilvsa <162391649+victorsilvsa@users.noreply.github.com>
2024-08-09 22:49:00 +0000

    teste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,49 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  pessoa={
+    foto:`https://cdn4.iconfinder.com/data/icons/gaming-70/130/game-31-512.png`,
+    nome:`Victor Silva 👩‍💻`,
+    objetivo:`Mestre Pokemon`,
+    contato: {
+      telefone:`333-666-777`,
+      email: `VictorSilva@Hotmail.com`,
+      github:`github.com/victorMestredosMestre`,
+      likendin:`likendin.com/victorMestredosMestre`,
+    },
 
+    softskills:[
+      'comunicação',
+      'Trabalho com Pokemon',
+      'Mestre da Liga', 
+      'Inovação',
+    ],
+    formacao_academica:[
+    {
+      ano_inicio:'2023',
+      ano_fim:'2025',
+      instituicao:'Etec Sales Gomes',
+      curso:'Desenvolvimento de Sistemas',
+    },
+    {
+      ano_inicio:'2040',
+      ano_fim:'2047',
+      instituicao:'Faculdade da Planta',
+      curso:'Meio Ambiente',
+    }
+    ],
+    meus_project:[
+      {
+        ano:'2047',
+        project_inject:'Etec de Portas Abertas',
+        desc: 'Nesse Projeto Irei apresentar Meus Pokemons Favaritos com Meu Grupo de Friends, Muito aesthetic, Finalidade: Nenhuma'
+      },
+      {
+        ano:'2047',
+        project_inject:'Etec de Portas Abertas',
+        desc: 'Nesse Projeto Irei apresentar Meus Pokemons Favaritos com Meu Grupo de Friends, Muito aesthetic, Finalidade: Nenhuma'
+      }
+    ]
+  }
   constructor(){ }
 }
