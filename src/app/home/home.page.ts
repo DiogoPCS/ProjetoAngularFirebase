fb0b0e6865e7d89b673b6ab9bd2deef817e44ca2
Bruno mendes <162513613+BrunoMendesMorais@users.noreply.github.com>
2024-08-13 23:50:48 +0000

    pagina de rede social

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,38 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // variaveis
+  pessoa = {
+    foto : 'https://avatars.githubusercontent.com/u/162513613?s=400&u=e0a9aa78b8458783075c9078ac379e73d298418d&v=4',
+    nome : 'Bruno Mendes de Morais',
+    objetivo : 'Programador Front-end',
+    contato : {
+      email: 'mendesdemoraisbruno@gmail.com',
+      telefone: '(15)052684899',
+      github:'https://github.com/BrunoMendesMorais',
+      linkedin:'brunomendesdemorais'
+    },
+    softskills:[
+      'comunicação',
+      'proatividade',
+      'trabalho em grupo',
+      'resiliencia'
+    ],
+    formacao:[
+      {
+        anoInicio:'2023',
+        anoFim:'2024',
+        instituicao:'Etec Sales Gomes',
+        curso:'Desenvolvimento de Sistema'
+      },
+      {
+        anoInicio:'2020',
+        anoFim:'2024',
+        instituicao:'Barão de surui',
+        curso:'Ensino médio'
+      }
+    ]
+  }
 
-  constructor(){ }
-}
+
+  }
