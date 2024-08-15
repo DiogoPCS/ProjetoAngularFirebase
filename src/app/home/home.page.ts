90446a232fecd73051df62056c1d8231ecf1136d
Alceu Mendes <54826468+AlceuNeto@users.noreply.github.com>
2024-08-15 22:25:56 +0000

    perfil angular alceu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,51 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  constructor(){ }
+perfil = {
+foto:'https://wallpapers-clan.com/wp-content/uploads/2022/07/kuromi-pfp-3.jpg',
+nome:'Alceu Neto',
+profissao: 'Programador',
+idioma:'Português',
+Cidade: 'Tatuí / SP',
+tempo: '2023',
+biografia : {
+  bio:"Programador desde 2023 e Músico Clássico"
+
+},
+estatisticas :{
+curtidas: '258.4k',
+seguindo: '35.9k',
+amigos: 25
+},
+postagens: [{
+  fotopost:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2pP2g7REZAmmQ3NHBVxBg-agdEW81i_xjmQ&s',
+  nomeperfilamigo:'Juvenal',
+  conteudo:'esse cara é muito ruim vai se fude'
+},
+{
+fotopost:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2pP2g7REZAmmQ3NHBVxBg-agdEW81i_xjmQ&s',
+nomeperfilamigo:'Alceu',
+conteudo:'pqp alguém deleta a conta desse cara'
+
+},
+{
+  fotopost:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2pP2g7REZAmmQ3NHBVxBg-agdEW81i_xjmQ&s',
+  nomeperfilamigo:'Alceu',
+  conteudo:'pqp alguém deleta a conta desse cara'
+  
+  },
+  {
+    fotopost:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2pP2g7REZAmmQ3NHBVxBg-agdEW81i_xjmQ&s',
+    nomeperfilamigo:'Alceu',
+    conteudo:'pqp alguém deleta a conta desse cara'
+    
+    },]
+
+
+
+
+
+
+}
+
 }
