41701130c7cf493e56beb56478e638e1216f3f4d
Artorias <126501493+Artorias-8546@users.noreply.github.com>
2024-08-15 16:57:58 +0000

    conexao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,7 +11,8 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
   recado = {
-    assunto: null
+    assunto: null,
+    mensagem: null
   }
 
   enviar(){
