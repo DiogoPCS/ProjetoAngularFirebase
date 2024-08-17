1b46cd3998bc73d1f0b75b004b5d8f892726100a
Gabriela Soares Frudeli <90211574+Frudeli@users.noreply.github.com>
2024-08-17 01:46:22 +0000

    atividade de hoje

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,92 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  constructor(){ }
+  // começar por aqui quando fazer um projeto
+
+  segments: string = 'usuarios';
+  
+  usuarios = [
+    {
+      nome: 'Selena Gomes',
+      idade: '21',
+      genero: 'masculino',
+      rotinas: [
+        {
+          dia: 'segunda-feira',
+          treinos: [
+            "Rosca Direta",
+            "Crucifixo",
+            'Remada Sumô',
+// array de string
+          ]
+        },
+
+        {
+          dia: 'Quarta-feira',
+          treinos: [
+            "Rosca Invertida",
+            "Pulley",
+            'Remada Sentada',
+// array de string
+          ]
+        },
+
+        {
+          dia: 'sexta-feira',
+          treinos: [
+            "Supino Inclinado",
+            "Bíceps Corta",
+            'Bíceps Hulk Invertido',
+// array de string
+          ]
+        },
+      ]
+
+
+    },
+    {
+      nome: 'Pedro Soares',
+      idade: '30',
+      genero: 'masculino',
+      rotinas: [
+        {
+          dia: 'Terça-feira',
+          treinos: [
+            "Prancha Redonda",
+            "Jumping Bom dia papai",
+            'Flexão de Costas',
+// array de string
+          ]
+        },
+
+        {
+          dia: 'Quinta-feira',
+          treinos: [
+            "Rosca em pé",
+            "Rosca scott barra",
+            'Bícepis Frances',
+// array de string
+          ]
+        },
+
+        {
+          dia: 'Sábado',
+          treinos: [
+            "Supino Inclinado",
+            "agachamento",
+            'Pula-corda',
+// array de string
+          ]
+        },
+      ]
+
+
+    }
+  ]
+
+  trocar(event: any){
+    this.segments = event.detail.value;
+    console.log(this.segments)
+
+  }
 }
