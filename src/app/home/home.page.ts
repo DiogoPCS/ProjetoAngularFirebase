c94158a333a8fadad877f96ff133ee933aa05d66
Thailane Santos <116779272+thailane-hub@users.noreply.github.com>
2024-08-20 23:53:58 +0000

    atualizacao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -86,6 +86,58 @@ export class HomePage {
     
   }
   ]
+  treinos = [
+    {
+      nome: 'Pulley',
+      series: 3,
+      foto: 'https://www.blog.treinoemalta.com.br/wp-content/uploads/2023/07/Triceps-Pulley-Barra.gif'
+    },
+    {
+      nome: 'Agachamento Sumô',
+      series: 3,
+      foto: 'https://www.mundoboaforma.com.br/wp-content/uploads/2021/09/agachamento-sumo-sem-halter.gif'
+    },
+    {
+      nome: 'Supino Reto',
+      series: 4,
+      foto: 'https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/supino-reto.gif'
+    },
+    {
+      nome: 'Remada Unilateral',
+      series: 4,
+      foto: 'https://static.wixstatic.com/media/2edbed_cf8feb6f79494833b887104bc358843d~mv2.gif'
+    },
+    {
+      nome: 'Levantamento Terra',
+      series: 4,
+      foto: 'https://www.hipertrofia.org/blog/wp-content/uploads/2017/11/barbell-deadlift.gif'
+    },
+    {
+      nome: 'Flexão de Braços',
+      series: 3,
+      foto: 'https://www.mundoboaforma.com.br/wp-content/uploads/2021/04/flexao-de-bracos.gif'
+    },
+    {
+      nome: 'Cadeira Abdutora',
+      series: 3,
+      foto: ''
+    },
+    {
+      nome: 'Rosca Direta',
+      series: 3,
+      foto: 'https://static.wixstatic.com/media/2edbed_8a612c33a1f649578ade7454653f7f30~mv2.gif'
+    },
+    {
+      nome: 'Prancha',
+      series: 3,
+      foto: 'https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/prancha-com-elevacao-das-pernas-prancha-aranha.gif'
+    },
+    {
+      nome: 'Extensão de Tríceps na Polia',
+      series: 3,
+      foto: 'https://www.mundoboaforma.com.br/wp-content/uploads/2021/07/triceps-no-pulley-atras-ca-cabeca.gif'
+    }
+  ];
 // acima variavel
 
   constructor() {}
