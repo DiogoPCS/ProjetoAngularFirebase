0551d5ae196c4d75fdfdee055a0a2dd6e7f90f88
Diogo <diogo.paulino.santos@hotmail.com>
2024-09-05 22:41:25 +0000

    remoção de código antigo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,39 +12,6 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  isLoading: boolean = false;
-  paises: any;
-
-  constructor(
-    private router: Router
-  ){
-    this.getPaisCode();
-  }
-  
-  getPaisCode() {
-    this.isLoading = true;
-
-    fetch('https://restcountries.com/v3.1/all?fields=name,flags,ccn3')
-    .then( dados => dados.json() ) // converter os dados recebidos para json
-    .then((dados) => {
-      console.log(dados);
-      this.paises = dados;
-    }) // fazer as ações desejadas com os dados
-    .catch((erro) => {
-      console.log(erro);
-    }) // exibir mensagens de erro
-    .finally(() => {
-      console.log('O processo foi finalizado!');
-      this.isLoading = false;
-    }) // fazer alguma coisas quando finalizar tudo
-  }
-
-  verDetalhesPais(code: string){
-    this.router.navigate(['/ver-detalhes-pais'],
-      {
-        state: { codigo: code}
-      }
-    );
-  }
+  constructor(){}
 
 }
