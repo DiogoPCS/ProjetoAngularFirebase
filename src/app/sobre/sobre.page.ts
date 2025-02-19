a1913e5b31af121896ce8c69356b6f4ba618c2a0
Thais <141294372+thamisd@users.noreply.github.com>
2025-02-19 18:15:10 +0000

    introdução ao angular

---
+++ src/app/sobre/sobre.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.page.html',
+  styleUrls: ['./sobre.page.scss'],
+})
+export class SobrePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
