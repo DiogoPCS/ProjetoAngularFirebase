322b18b781bde81500f186ef749ca1f7351c3e99
Lucas Mota <141058152+LucasMota630@users.noreply.github.com>
2025-02-19 19:26:39 +0000

    configuração do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAgqpnXNxZ8abif8ssQq_C2s998w7jZb68",
-    authDomain: "api-aula-d6451.firebaseapp.com",
-    projectId: "api-aula-d6451",
-    storageBucket: "api-aula-d6451.appspot.com",
-    messagingSenderId: "48648388488",
-    appId: "1:48648388488:web:dc73e0a1496a52cd4b843d",
-    measurementId: "G-CR3MDWMWJF"
+    apiKey: "AIzaSyC8_5tiKy0pHyAWH_HdxzEoJ4jAg9DwLfc",
+  authDomain: "aula-78094.firebaseapp.com",
+  projectId: "aula-78094",
+  storageBucket: "aula-78094.firebasestorage.app",
+  messagingSenderId: "1082394111261",
+  appId: "1:1082394111261:web:2f7295078189b09415c5f4",
+  measurementId: "G-JX56FZSG27"
   },
   production: true
 };
