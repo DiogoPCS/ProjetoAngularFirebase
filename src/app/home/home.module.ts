b117c4976dba17524cf4eb39cf83f942aa8e8fa0
Bruno de Oliveira Nunes <141058085+BrunoNunes100@users.noreply.github.com>
2025-02-19 17:43:57 +0000

    aula 19-02

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { RouterLink } from '@angular/router';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    RouterLink
   ],
   declarations: [HomePage]
 })
