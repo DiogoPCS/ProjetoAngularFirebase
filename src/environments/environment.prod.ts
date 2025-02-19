dda8645af10ce8d2d36cbc2d4daf61b13630782e
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-02-19 16:26:35 -0300

    Configuração do Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAgqpnXNxZ8abif8ssQq_C2s998w7jZb68",
-    authDomain: "api-aula-d6451.firebaseapp.com",
-    projectId: "api-aula-d6451",
-    storageBucket: "api-aula-d6451.appspot.com",
-    messagingSenderId: "48648388488",
-    appId: "1:48648388488:web:dc73e0a1496a52cd4b843d",
-    measurementId: "G-CR3MDWMWJF"
+    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
+    authDomain: "aula-3e82a.firebaseapp.com",
+    projectId: "aula-3e82a",
+    storageBucket: "aula-3e82a.firebasestorage.app",
+    messagingSenderId: "258284807710",
+    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
+    measurementId: "G-X8L8GS7897"
   },
   production: true
 };
