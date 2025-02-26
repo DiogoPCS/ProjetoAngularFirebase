716fb2f6a1224f5ba50b9210d96a70caaf78f1b3
Anthonyrr07 <148653603+Anthonyrr07@users.noreply.github.com>
2025-02-26 19:29:20 +0000

    implementação dos cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,26 +1,39 @@
-import { ThisReceiver } from "@angular/compiler";
-import { CrudService } from "../services/crud.service";
+import { Component } from '@angular/core';
+import { AuthenticateService } from '../services/auth.service';
+import { CrudService } from '../services/crud.service';
+import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
+import { MessageService } from '../services/message.service';
+import { Router } from '@angular/router';
 
 @Component ({
   selector: 'app-home', 
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.css'],
+  styleUrls: ['home.page.scss'],
 })
 
 export class HomePage{
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
-
-  constructor(
-    public crudService:CrudService
-    $pokemon = new Pokemon()
-
-   ) { } 
+  url: string = `https://restcountries.com/v3.1/all?fields=name,flags,borders,currencies`;
+  is_loading: boolean = false;
+  dados: any = {};
   
+  constructor(){
+    this.is_loading = true;
+    fetch(this.url)
+    .then(dados => dados.json())
+    .then(dados =>{
+      console.log(dados);
+      this.dados = dados;
+    })
+    .catch(_ => {
+      console.log(_);
+    })
+    .finally(()=>{
+      this.is_loading = false;
+      console.log('requisição finalizada');
+    })
+    
+   }
 
-  enviar (){
-    this.crudService.insert(this.pokemon, 'pokemons')
-  }
+  
+  
 }
