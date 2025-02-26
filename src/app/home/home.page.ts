f6cfccd03b16c3711633a5be945199ed5f003aef
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-02-26 14:47:17 -0300

    implementação dos cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthenticateService } from '../services/auth.service';
-import { CrudService } from '../services/crud.service';
-import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
-import { MessageService } from '../services/message.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,17 +7,24 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
+  url: string = `https://restcountries.com/v3.1/all?fields=name,flags`;
+  is_loading: boolean = false;
+  dados: any = {};
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+  constructor( ) { 
+    fetch(this.url)
+    .then(dados => dados.json())//converter os dados
+    .then(dados => {
+      console.log(dados);
+      this.dados = dados;
+    })//exibir os dados
+    .catch(_ => {
+      console.log(_);
+    })//tratar possíveis erros
+    .finally(()=>{
+      console.log('Requisição Finalizada!');
+    })//exibir que finalizou
   }
+
 
 }
