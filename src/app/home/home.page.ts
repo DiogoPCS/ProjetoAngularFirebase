d51d2e3b69f0a529f52ac3b736f578c7379e7326
Anthonyrr07 <148653603+Anthonyrr07@users.noreply.github.com>
2025-02-26 18:03:04 +0000

    mudança

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
-import { AuthenticateService } from '../services/auth.service';
-import { CrudService } from '../services/crud.service';
-import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
-import { MessageService } from '../services/message.service';
-import { Router } from '@angular/router';
+import { ThisReceiver } from "@angular/compiler";
+import { CrudService } from "../services/crud.service";
 
-@Component({
-  selector: 'app-home',
+@Component ({
+  selector: 'app-home', 
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.css'],
 })
-export class HomePage {
 
-  constructor( ){ }
+export class HomePage{
+  pokemon:any = {
+    nome: null,
+    poder: null
+  };
 
+  constructor(
+    public crudService:CrudService
+    $pokemon = new Pokemon()
+
+   ) { } 
+  
+
+  enviar (){
+    this.crudService.insert(this.pokemon, 'pokemons')
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAgqpnXNxZ8abif8ssQq_C2s998w7jZb68",
-    authDomain: "api-aula-d6451.firebaseapp.com",
-    projectId: "api-aula-d6451",
-    storageBucket: "api-aula-d6451.appspot.com",
-    messagingSenderId: "48648388488",
-    appId: "1:48648388488:web:dc73e0a1496a52cd4b843d",
-    measurementId: "G-CR3MDWMWJF"
+    apiKey: "AIzaSyCvryFkbfEY1mqwdDEw-KB6wy5c_ICLQEk",
+  authDomain: "aula-296a6.firebaseapp.com",
+  projectId: "aula-296a6",
+  storageBucket: "aula-296a6.firebasestorage.app",
+  messagingSenderId: "714196717980",
+  appId: "1:714196717980:web:1c0a49b544ad47451cf859",
+  measurementId: "G-7P3LM16EPW"
   },
   production: true
 };
