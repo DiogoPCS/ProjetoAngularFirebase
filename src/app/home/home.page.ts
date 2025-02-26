7df36f7f937f9fc38ac0e24b3a4dce5a7df9d8a3
LettyCampbell <leticiacampbellgoncalves@gmail.com>
2025-02-26 17:47:17 +0000

    implementação dos cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,28 +1,29 @@
-import { Component } from '@angular/core';
-import { AuthenticateService } from '../services/auth.service';
-import { CrudService } from '../services/crud.service';
-import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
-import { MessageService } from '../services/message.service';
-import { Router } from '@angular/router';
-
+import { Component} from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
+  url: string = `https://restcountries.com/v3.1/all?fields=name,flags`;
+  is_loading: boolean = false;
+  dados: any = null;
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+  constructor(){
+    fetch(this.url)
+    .then(dados => dados.json() )//converter os dados
+    .then(dados => {
+      console.log(dados);
+      this.dados = dados;
+    })// exibir os dados
+    .catch(_ => {
+      console.log(_);
+    })//tratar possiveis erros
+    .finally(() => {
+      console.log('Requisição Finalizada!');
+    })//exibir que finalizou
   }
 
 }
