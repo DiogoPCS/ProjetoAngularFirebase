3b03cbf42b9a9fda18297a4e84956eb903b678c1
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-03-12 14:48:36 -0300

    implementação rede social

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,52 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
+  perfil: any = {
+    foto: null,
     nome: null,
-    poder: null
+    profissao: null,
+    nome_usuario: null,
+    localidade: null,
+    data_inicio: null,
+    biografia: null,
+    estatisticas: {
+      curtidas: 0,
+      seguidores: 0,
+      amigos: 0
+    },
+    postagens:[
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1NaRs46tXL_CDdRMWZKg-RAleTur_T0E8xw&s',
+        nome: 'Carlos Antônio',
+        nome_usuario: '@carlosantonio',
+        texto: 'Texto de exemplo de postagem',
+        data: '12/03/2025'
+      },
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1NaRs46tXL_CDdRMWZKg-RAleTur_T0E8xw&s',
+        nome: 'Carlos Antônio',
+        nome_usuario: '@carlosantonio',
+        texto: 'Outro exemplo de texto de postagem',
+        data: '13/03/2025'
+      },
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1NaRs46tXL_CDdRMWZKg-RAleTur_T0E8xw&s',
+        nome: 'Carlos Antônio',
+        nome_usuario: '@carlosantonio',
+        texto: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, id provident nostrum natus non vitae, doloribus magni animi mollitia, at tempore reprehenderit. Sint pariatur porro fuga modi, nemo quas. Quis.',
+        data: '14/03/2025'
+      },
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1NaRs46tXL_CDdRMWZKg-RAleTur_T0E8xw&s',
+        nome: 'Carlos Antônio',
+        nome_usuario: '@carlosantonio',
+        texto: 'Hello World!!!',
+        data: '15/03/2025'
+      }
+    ]
   };
+  
+  constructor( ){ }
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
-  }
-
+  
 }
