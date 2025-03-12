6a3798c16cbfd4c93d0d140b8d0f1745c49a3ed3
vini6161610 <141057880+vini6161610@users.noreply.github.com>
2025-03-12 19:24:59 +0000

    social

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,19 +11,48 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-pokemon:any = {
-  nome: null,
-  poder: null
-};
-  constructor(
-    public crudService: CrudService
+  perfil: any ={
+    foto: null,
+    nome: null,
+    profissao:null,
+    nome_usuario:null,
+    idioma:null,
+    localidade:null,
+    data_inicio:null,
+    biografia:null,
+    estatisticas:{
+      curtidas: 0,
+      seguindo: 0,
+      amigos:0
+    },
+    postagem:[
+      {
+        foto:'https://pbs.twimg.com/media/FN7ozAFVUAEgWnf.jpg:large',
+        nome:'vinicius',
+        nome_usuario:'@vini616',
+        texto:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, facere cumque. Molestias quo nobis dicta voluptatem obcaecati, quasi tempora alias, laborum voluptas molestiae qui! Labore illum corporis ipsum veniam quam?',
+        data:'12/03/2025'
+      },
+      {
+        foto:'https://preview.redd.it/sister-friede-build-guide-v0-i5qsd41poj2e1.jpg?width=310&format=pjpg&auto=webp&s=c5324ea455ac3d60deb3f73ad2b24ef0eb3f752e',
+        nome:'vinicius',
+        nome_usuario:'@vini616',
+        texto:'irmã frige é impossivel',
+        data:'12/03/2025'
+      },
+      {
+        foto:'https://i.ytimg.com/vi/fBnZa0kJFJQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLADqHpnh17kXx3bLQ1_cVK9dowMqw',
+        nome:'vinicius',
+        nome_usuario:'@vini616',
+        texto:'radhan é easy',
+        data:'12/03/2025'
+      },
 
-  ){
+    ]
+  }
 
-  }
-  enviar(){
-this.crudService.insert(this.pokemon, 'pokemons')
-  }
+
+
 };
 
 
