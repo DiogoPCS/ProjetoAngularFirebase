0ab113b6a48a41c96dcc1accc29373c184471e01
Thg <141058080+Thiago200888@users.noreply.github.com>
2025-03-12 17:48:38 +0000

    implementação rede social

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,56 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
- pokemon:any = {
-  nome: null,
-  poder: null
+ perfil: any = {
+   nome: null,
+   profissao: null,
+   nome_usuario: null,
+   idioma: null,
+   localidade: null,
+   data: null,
+   biografia: null,
+
+   estatistiscas: {
+    curtida: 0,
+    seguidores: 0,
+    amigos: 0
+   },
+
+   postagens:[
+    {
+      foto: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fperfisecross.fandom.com%2Fpt-br%2Fwiki%2FSousuke_Aizen&psig=AOvVaw2LWisL0wkHf57Iu3phMphI&ust=1741885141043000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCJkYmChYwDFQAAAAAdAAAAABAS',
+      nome: 'Carlos Antonio',
+      nome_usuario: '@carlosantonio',
+      texto: 'thg',
+      data: '12/03/2025' 
+   },
+
+   {
+    foto: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fperfisecross.fandom.com%2Fpt-br%2Fwiki%2FSousuke_Aizen&psig=AOvVaw2LWisL0wkHf57Iu3phMphI&ust=1741885141043000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCJkYmChYwDFQAAAAAdAAAAABAS',
+    nome: 'Carlos Antonio',
+    nome_usuario: '@carlosantonio',
+    texto: 'O',
+    data: '13/03/2025' 
+ },
+
+ {
+  foto: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fperfisecross.fandom.com%2Fpt-br%2Fwiki%2FSousuke_Aizen&psig=AOvVaw2LWisL0wkHf57Iu3phMphI&ust=1741885141043000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCJkYmChYwDFQAAAAAdAAAAABAS',
+  nome: 'Carlos Antonio',
+  nome_usuario: '@carlosantonio',
+  texto: 'Cara',
+  data: '14/03/2025' 
+ },
+
+ {
+  foto: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fperfisecross.fandom.com%2Fpt-br%2Fwiki%2FSousuke_Aizen&psig=AOvVaw2LWisL0wkHf57Iu3phMphI&ust=1741885141043000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCJkYmChYwDFQAAAAAdAAAAABAS',
+  nome: 'Carlos Antonio',
+  nome_usuario: '@carlosantonio',
+  texto: 'Perfeito',
+  data: '15/03/2025' 
+ },
+   ]
  };
 
-  constructor(
-    public crudService: CrudService
-   ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons')
-  }
+  constructor(){ }
 
 }
