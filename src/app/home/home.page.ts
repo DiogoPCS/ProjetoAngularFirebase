49402420ef2829accee909bb844f72d6e0cfbd19
Rafael1172 <141057789+Rafael1172@users.noreply.github.com>
2025-03-12 19:24:13 +0000

    manasjndjkbn

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,45 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
+  perfil: any = {
+    foto: null,
     nome: null,
-    poder: null
-  };
-
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+    profissão: null,
+    nome_usuario: null,
+    idioma: null,
+    localidade: null,
+    data_inicio: null,
+    biografia: null,
+    estatiscas: {
+      curtidas: 0,
+      seguindo: 0,
+      amigos: 0
+    },
+    postagens: [
+      {
+        foto: 'https://darksouls-portuguese.wiki.fextralife.com/file/Dark-Souls---Portuguese/fatty_mc_fatso.jpg',
+        nome: 'Pikimon Alberto',
+        nome_usuario: '@pikimonalberto',
+        texto: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, deleniti ea omnis ab ducimus rem nulla modi aut architecto, non neque dolorum, consectetur exercitationem at accusantium possimus quibusdam. Voluptatibus, vitae.',
+        data: '12/03/2025 14:00'
+      },
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnx32jbe7ZsiBO-VDnyHrEAABjYYR_15R2ow&s',
+        nome: 'Pikimon Alberto',
+        nome_usuario: '@pikimonalberto',
+        texto: 'otário kkkkkkkkkkkk',
+        data: '12/03/2025 14:00'
+      },
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAn4P7ANMqVfs4Ldo6WoCwC0V90gbr9rhUjg&s',
+        nome: 'Pikimon Alberto',
+        nome_usuario: '@pikimonalberto',
+        texto: 'otário kkkkkkkkkkkk',
+        data: '12/03/2025 14:00'
+      }
+    ]
   }
 
-}
+  constructor( ){ }
+
+  }
