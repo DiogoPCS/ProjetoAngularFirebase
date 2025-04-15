e103ad9844d64875d51aebbaabb9c4dea7e4e401
Anthonyrr07 <148653603+Anthonyrr07@users.noreply.github.com>
2025-04-15 18:00:06 +0000

    atualizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -12,45 +13,34 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  perfil: any = {
-    foto: null,
-    nome: null,
-    profissao: null,
-    nome_usuario: null,
-    idioma: null,
-    localidade: null,
-    data_inicio: null,
-    biografia: null,
-    estatisticas: {
-      curtidas: 0,
-      seguindo: 0,
-      amigos: 0
-    },
-    postagens: [
-      {
-        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqyZDSyazgi2SYkrOX_KXLu9_zEn6eyNTWyw&s',
-        nome: 'Carlos Antônio',
-        nome_usuario: '@carlosantonio',
-        texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus placeat unde a quod exercitationem saepe obcaecati ullam corrupti quos! Excepturi est nemo mollitia similique maiores voluptatibus tempore provident corporis quia?',
-        data: '12/03/2025 14:00'
-      },
-      {
-        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqyZDSyazgi2SYkrOX_KXLu9_zEn6eyNTWyw&s',
-        nome: 'Carlos Antônio',
-        nome_usuario: '@carlosantonio',
-        texto: 'Outro exemplo de Postagem!!!',
-        data: '12/03/2025 14:10'
-      },
-      {
-        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqyZDSyazgi2SYkrOX_KXLu9_zEn6eyNTWyw&s',
-        nome: 'Carlos Antônio',
-        nome_usuario: '@carlosantonio',
-        texto: 'Hello World!',
-        data: '12/03/2025 14:30'
-      },
-    ]
+  data_selecionada: string = '';
+  data_inicial: any;
+  data_atual: any;
+  dias_vividos: number = 0;
+
+  constructor(
+  ){ }
+
+  definirData(event: any){
+    console.log(event.detail.value);
+    this.data_selecionada = event.detail.value;
   }
 
-  constructor( ){ }
+  // Calculando dias vividos
+  definirDataInicial(event: any){    
+      this.data_inicial = event.detail.value;
+    }
 
-}
+    definirDataAtual(event: any){
+      this.data_atual = event.detail.value;
+      this.calcularDiasVividos();
+    }
+  
+    calcularDiasVividos(){
+      const d1 = moment(this.data_inicial);
+      const d2 = moment(this.data_atual);
+      this.dias_vividos = d2.diff(d1, 'days');
+      
+    }
+
+  }
