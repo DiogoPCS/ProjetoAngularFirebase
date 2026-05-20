8ef1007bff7f76d5eee6de480ab572ee8f40053f
MatheusNMoraes <matheusnunesdemoraes432@gmail.com>
2026-05-20 12:25:04 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,8 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
-
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+  mensagem(){
+    alert('vaiiii neymaaaaaaaarrrrrrrr')
   }
 
 }
